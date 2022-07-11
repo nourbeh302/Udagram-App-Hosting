1426715025fd4d44d09a630d29b44c248105f951
NourEldin Ahmed Samir <NourEldin20201895@fci.helwan.edu.eg>
2022-07-11 15:15:52 +0200

    Deploying the production front-end code

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0", /* Elastic Beanstalk URL */
+  apiHost: "http://udagramnodejs-env.eba-hwm4ynqs.us-east-1.elasticbeanstalk.com/api/v0", /* Elastic Beanstalk URL */
 };
 
 /*
